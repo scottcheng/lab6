186821938b4f04315be3879efede8df4d0a7d7cb
Scott Cheng <me@scottcheng.com>
2014-02-12 14:25:37 -0800

    Finished lab6

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,6 +27,15 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	var $details = $(this).siblings('.details');
+	var url = '/project/' + idNumber;
+	console.log(url);
+	$.get(url, function(data) {
+		$details.html('<img class="detailsImage" src="' + data.image + '" />' +
+			'<div class="date">' + data.date + '</div>' +
+			'<div class="summary">' + data.summary + '</div>');
+	});
 }
 
 /*
@@ -35,4 +44,14 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+
+	$.get('/palette', function(data) {
+		var colors = data.colors.hex;
+
+		$('body').css('background-color', colors[0]);
+		$('.thumbnail').css('background-color', colors[1]);
+		$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+		$('p').css('color', colors[3]);
+		$('.project img').css('opacity', .75);
+	});
 }
